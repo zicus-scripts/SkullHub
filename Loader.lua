cb93afa54340861424c3e99ede49f50ceb895a52
1Toxin <117609107+1Toxin@users.noreply.github.com>
2023-07-19 18:22:19 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,5 @@
 if game.PlaceId == "13127800756" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/zicus-scripts/SkullHub/main/AWS.lua"))()
-elseif game.PlaceId == "4924922222" then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zicus-scripts/SkullHub/main/AWS.lua"))()
 else
   game.Players.LocalPlayer:Kick("Skull Hub | Invalid Game")
 end
