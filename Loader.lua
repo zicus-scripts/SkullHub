596559ba00f49cdfedaa1d02de432194fa6f6b41
1Toxin <117609107+1Toxin@users.noreply.github.com>
2023-07-23 16:18:43 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,245 @@
-if game.PlaceId == 13127800756 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zicus-scripts/SkullHub/main/AWS.lua"))()
-elseif game.PlaceId == 4924922222 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zicus-scripts/SkullHub/main/Brookhaven.lua"))()
-else
-  game.Players.LocalPlayer:Kick("Skull Hub | Invalid Game")
-end
+getgenv().Config = {}
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "SKULL - Arm Wrestle Simulator", HidePremium = false, SaveConfig = true, ConfigFolder = "SKULL", IntroText = "SKULL Hub", IntroIcon = "rbxassetid://10851325055"})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW Screech Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "COPIED IT TO YOUR CLIPBOARD",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+OrionLib:MakeNotification({
+    Name = "NEW DISCORD JOIN",
+    Content = "Join the NEW SKULL Hub Discord",
+    Image = "rbxassetid://10851325055",
+    Time = 10
+})
+
+local Tab = Window:MakeTab({
+	Name = "NEW DISCORD JOIN",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Copy new discord invite",
+	Callback = function()
+      		setclipboard("https://discord.gg/TYVp6X8WVF")
+  	end    
+})
+
+
+
+setclipboard("https://discord.gg/TYVp6X8WVF")
