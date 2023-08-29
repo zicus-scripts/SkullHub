5d185b694f6af4d8ec9d8f890b8dc44ce4d33dca
1Toxin <117609107+1Toxin@users.noreply.github.com>
2023-08-29 10:30:07 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,132 @@
-if game.PlaceId == 13127800756 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zicus-scripts/SkullHub/main/AWS.lua"))()
-elseif game.PlaceId == 4924922222 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zicus-scripts/SkullHub/main/Brookhaven.lua"))()
-else
-  game.Players.LocalPlayer:Kick("Skull Hub | Invalid Game")
-end
+getgenv().Config = {}
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Flame - Arm Wrestle Simulator", HidePremium = false, SaveConfig = true, ConfigFolder = "FlameHub", IntroText = "Flame Hub", IntroIcon = "rbxassetid://12517360827"})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord to get the newest script!!!!",
+    Image = "rbxassetid://12517360827",
+    Time = 5
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "This script wont be as updated",
+    Image = "rbxassetid://12517360827",
+    Time = 5
+})
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+OrionLib:MakeNotification({
+    Name = "Flame Hub",
+    Content = "Join the Discord server",
+    Image = "rbxassetid://12517360827",
+    Time = 500
+})
+
+
+local Home = Window:MakeTab({
+	Name = "Home",
+	Icon = "rbxassetid://12517360827",
+	PremiumOnly = false
+})
+
+local UpdateLog = Home:AddSection({
+	Name = "Section"
+})
+
+UpdateLog:AddButton({
+	Name = "Join the discord for new script",
+	Callback = function()
+		setclipboard("https://discord.gg/S87MR2VNse")
+	end
+})
+
+UpdateLog:AddParagraph("Update Log","New discord join it")
+UpdateLog:AddParagraph("Credits","Script made by Toxin")
+
+UpdateLog:AddButton({
+	Name = "Copy Discord Invite",
+	Callback = function()
+		setclipboard("https://discord.gg/S87MR2VNse")
+	end
+})
+
+UpdateLog:AddButton({
+	Name = "Copy Discord Invite",
+	Callback = function()
+		setclipboard("https://discord.gg/S87MR2VNse")
+	end
+})
